19dc720db174f460bd93e163cf04e2390cc018f6
Eivind Joakim Sandell <eivindjs@stud.ntnu.no>
2016-03-15 21:40:55 +0100

    Mer gui greier. package er satt tilbake til trainingdiary.src.base så for dere som ikke bruker eclipse bvare gjør den om til base

--- trainingdiary/src/base/Controller.java
+++ trainingdiary/src/base/Controller.java
@@ -1,4 +1,4 @@
-package base;
+package trainingdiary.src.base;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
@@ -9,26 +9,50 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import trainingdiary.src.base.*;
+import trainingdiary.src.base.Session;
+import trainingdiary.src.base.Main;
 
 public class Controller {
 	
+	private Main main;
+	
+	/*
+	 * Creates the table where we push the info from the db
+	 */
 	@FXML
 	private TableView<Session> sessionTable;
 	@FXML
-	private TableView<Session, String> nameCol;
+	private TableColumn<Session, String> nameCol;
 	@FXML
-	private TableView<Session, String> formCol;
+	private TableColumn<Session, String> formCol;
 	@FXML
-	private TableView<Session, String> resultCol;
+	private TableColumn<Session, String> resultCol;
 	@FXML
-	private TableView<Session, String> exerciseCol;
+	private TableColumn<Session, String> exerciseCol;
 
 	public Button SaveButton;
 	String timeRegex = "^(1?[0-9]|2[0-3]):[0-5][0-9]$";
+	
+	
+	/*
+	 * Initializes the controller class. This is loaded automaticly
+	 */
+	public void initialize(){
+		nameCol.setCellValueFactory(cellData -> cellData.getValue().nameProperty());
+	}
+	
+	/*
+	 * Called my main to set observerlist
+	 */
+	public void setMain(Main main){
+		this.main = main;
+		sessionTable.setItems(main.getSessionData());
+	}
+	
 	
 	/** 
 	 * Triggers when save in diary is pressed
--- trainingdiary/src/base/Session.java
+++ trainingdiary/src/base/Session.java
@@ -1,22 +1,35 @@
 package trainingdiary.src.base;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+
+/*
+ * Creates the session object where we will save the db info when the app is running
+ */
 public class Session {
 
-	private String name;
+	private StringProperty name;
 	private String form;
 	private String result;
 	private String exercise;
 	
-	
+	/** 
+	 * Constructor that makes a new session
+	 * @param Name off session
+	 * @param users form string from 1 to 10
+	 * @param users result string from 1 to 10
+	 * @param string that is a list of exercises
+	 */
 	public Session(String name, String form, String result, String exercise){
-		this.name = name;
+		this.name = new SimpleStringProperty(name);
 		this.form = form;
 		this.result = result;
 		this.exercise = exercise;
 	}
 
 
-	public String getName() {
+	public StringProperty nameProperty() {
 		return name;
 	}
 
