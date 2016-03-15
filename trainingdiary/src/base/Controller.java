730c4591e196f9d52847ee8c1541e7d48ffc55e1
Eivind Joakim Sandell <eivindjs@stud.ntnu.no>
2016-03-15 13:04:38 +0100

    fikset controller bug

--- trainingdiary/src/base/Controller.java
+++ trainingdiary/src/base/Controller.java
@@ -13,39 +13,12 @@ import javafx.scene.control.TextField;
 public class Controller {
 
 	public Button LagreKnapp;
-	public TextField rom;
-	public DatePicker dato;
-	public TextField fra;
-	public TextField til;
-	public TextField rep;
-	public TextField slutt;
-	public TextArea formål;
 
 	
 	
 	public void handleButton(){
-		String romNr = String.valueOf(rom.getText());
-		LocalDate datoVal = dato.getValue();
-		String fraString = String.valueOf(fra.getText());
-		String tilString = String.valueOf(til.getText());
-		String repString = String.valueOf(rep.getText());
-		String sluttString = String.valueOf(slutt.getText());
-		String formålString = String.valueOf(formål.getText());
 		
-		String[] splitedRom = romNr.split("\\s+");
-		if (splitedRom[1].matches(".*\\d+.*")){
-			System.out.println("Rom akseptert");
-		}
-		else{
-			System.out.println("Nope");
-		}
-		
-		if (fraString.contains(":")){
-			System.out.println("Riktig fra tidspunkt");
-		}
-		if (tilString.contains(":")){
-			System.out.println("Riktig til tidspunkt");
-		}
+		System.out.print("hey");
 		
 		
 	}
--- trainingdiary/src/base/Main.java
+++ trainingdiary/src/base/Main.java
@@ -1,8 +1,28 @@
 package trainingdiary.src.base;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 
-    public static void main(String[] args) {
-	// write your code here
-    }
+
+public class Main extends Application {
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("gui.fxml"));
+		primaryStage.setTitle("gui");
+		primaryStage.setScene(new Scene(root, 600, 600));
+		primaryStage.show();
+		
+		
+	}
+		
+	
+	
 }
